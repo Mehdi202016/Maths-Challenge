ec87ce35cf61f61b1c976aa9a6ef6f5a03fa7df0
Mehdi202016 <https://github.com/Mehdi202016>
2022-05-29 13:06:07 +0100

    Casual game

---
+++ Assets/Scripts/MainMenuController.cs
@@ -0,0 +1,68 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+
+public class MainMenuController : MonoBehaviour {
+
+    [SerializeField]
+    private Button musicBtn;
+
+    [SerializeField]
+    private Sprite[] musicBtnSprite;
+
+    private AudioSource clickSound;
+
+
+	void Start ()
+    {
+        if (GameManager.singleton.isMusicOn)
+        {
+            AudioListener.volume = 1;
+            musicBtn.image.sprite = musicBtnSprite[1];
+        }
+        else
+        {
+            AudioListener.volume = 0;
+            musicBtn.image.sprite = musicBtnSprite[0];
+        }
+        clickSound = GetComponent<AudioSource>();
+	}
+	
+
+    public void PlayButton()
+    {
+        SceneManager.LoadScene("ModeSelector");
+        clickSound.Play();
+    }
+
+
+    public void QuitButton()
+    {
+        Application.Quit();
+        //sound mli katwarek 3la button
+        clickSound.Play();
+    }
+
+    public void MusicButton()
+    {
+        clickSound.Play();
+
+        if (GameManager.singleton.isMusicOn)
+        {
+            AudioListener.volume = 0;
+            musicBtn.image.sprite = musicBtnSprite[0];
+            GameManager.singleton.isMusicOn = false;
+            GameManager.singleton.Save();
+        }
+        else
+        {
+            AudioListener.volume = 1;
+            musicBtn.image.sprite = musicBtnSprite[1];
+            GameManager.singleton.isMusicOn = true;
+            GameManager.singleton.Save();
+        }
+    }
+
+}
